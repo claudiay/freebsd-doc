8a547dc50548b5033e8d6ba108d2dd8ed34855b6
Claudia <c@luperlabs.com>
2015-02-20 00:16:45 -0800

    Moved all the .xml article files down one directory, and made them into .html. They need to be later formatted into the flask templating style.
    There were some odd files, so those were also saved. Putting off
    deciding what to do with them until later.

--- migration_scripts/move_xml.py
+++ migration_scripts/move_xml.py
@@ -1,4 +1,34 @@
 # Create a script move all xml files down a directory into the same level.
 # Later on these should be turned into html templates.
 
+import os
+import shutil
+templates = 'website/templates/'
+languages = os.listdir(templates)
+articles = {}
+except_folders = ('base_templates',)
+
+for lang in languages:
+    if lang not in except_folders:
+        print "Moving articles for %s" % lang
+        try:
+            articles[lang] = os.listdir('%s%s/articles/' % (templates, lang))
+            for subject in articles[lang]:
+                if (len(subject) > 5) and (subject[-5:] != '.html') and \
+                    subject not in ('Makefile', 'Makefile.inc'):
+                    search_path = '%s%s/articles/%s/' % (templates, lang, subject)
+                    new_path = '%s%s/articles/%s.html' % (templates, lang, subject)
+                    if ('article.xml' in os.listdir(search_path)):
+                        print "Going to move file to %s and delete %s." % (new_path, search_path)
+                        os.rename('%sarticle.xml' % search_path, new_path)
+                        print 'renamed'
+                        shutil.rmtree(search_path)
+                        print 'deleted'
+                        continue
+        except Exception as e:
+            print e
+            pass
+
+
+
 
